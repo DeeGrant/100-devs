e5b55862a909df18955077b32638430178f3c3a6
Walter Grant <WalterGrant@gmail.com>
2021-02-25 23:25:29 -0600

    html and css for tic-tac-toe

---
+++ class25-materials/tic-tac-toe/js/main.js
@@ -0,0 +1,9 @@
+let Board = {
+    spaces: ['x', 'o', '', '', '', ''],
+    playerTurn: true, // true = x, false = 0
+    // number of turns?
+    calculateForWin() {
+        // calculate if there is win or draw
+        // output message
+    }
+}
