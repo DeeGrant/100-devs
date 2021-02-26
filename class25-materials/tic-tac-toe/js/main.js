8240b1c2aa5a905cbcba1f61eb8e534b45a64326
Walter Grant <WalterGrant@gmail.com>
2021-02-26 09:56:55 -0600

    tic-tac-toe todo notes

--- class25-materials/tic-tac-toe/js/main.js
+++ class25-materials/tic-tac-toe/js/main.js
@@ -1,9 +1,36 @@
 let Board = {
     spaces: ['x', 'o', '', '', '', ''],
-    playerTurn: true, // true = x, false = 0
-    // number of turns?
-    calculateForWin() {
+    player: true, // true = x, false = 0
+    turn: 0, // number of turns?
+    isGameFinished() {
         // calculate if there is win or draw
+
+        // search win arrays for index of space just placed
+
         // output message
     }
 }
+// win condition object?
+// let win = {win: [[0,1,2],[3,4,5],...]}
+
+// start function
+function initPage() {
+    // initialize the needed stuff
+}
+// separate init game function? (eventually...)
+
+
+// TODO
+// convert object ot JSON
+// load and save to localStorage
+// restart game button
+//
+// Game series
+// keep a running track of the score?
+//
+// Create a tic-tac-toe bot?
+// difficulty levels?
+
+
+// HTML & CSS
+// Have entire game scale with browser size (all relative sizing)
