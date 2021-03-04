13a59add8edff00ce03678c5663e7ec19b0b023e
Walter Grant <WalterGrant@gmail.com>
2021-03-04 17:29:49 -0600

    tic-tac-toe progress - start checking for win

--- class25-materials/tic-tac-toe/js/main.js
+++ class25-materials/tic-tac-toe/js/main.js
@@ -1,91 +1,92 @@
-class Space {
-    get dom() {
-        return this._dom;
-    }
-    set dom(value) {
-        this._dom = value;
-    }
-    get id() {
-        return this._id;
-    }
-    get val() {
-        return this._val;
-    }
-    set val(value) {
-        // check that value not empty string
-        this._val = value;
-        this._dom.innerText = value
-    }
+// TODO clean up
 
-    constructor(dom, id, val) {
-        this._dom = dom;
-        this._id = id;
-        this._val = val;
-    }
-    addClickEvent() {
-        this._dom.addEventListener('click', () => clickSpace(this._id)) // still need 'isXsTurn' ...
-    }
-}
-
-// make real class
 class Board {
     constructor() {
+        this._gameContinues = true
+        this._isXsTurn = true
+        this._turn = 1
+        this._winConditions = [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,4,8],
+            [2,4,6],
+        ]
 
-    }
-}
-
-let board = {
-    initBoard() {
+        // initialize spaces
         for (let i = 0; i < 9; i++) {
             let id = 'space' + i
             this[id] = document.getElementById(id)
             this[id].addEventListener('click', (e) => {
+                // the gameplay
                 let id = e.currentTarget.id
-                console.log(id)
-                let xo = this.XorO()
-                console.log(xo)
-                this[id].innerText = xo
+
+                if (this[id].innerText === '') {
+                    console.log(id)
+                    this[id].innerText = this.XorO()
+
+                    this.nextTurn()
+                } else {
+                    alert('That space is already taken. Choose a different space.')
+                }
             })
         }
-    },
-    _isXsTurn: true,
+    }
+
     XorO() {
         let ret = this._isXsTurn ? 'X' : 'O'
-        this._isXsTurn = !this._isXsTurn
-        this.nextTurn()
+        console.log(ret)
         return ret
-    },
-    turnsLeft: 9, // number of turns. 0 -> 9 or 9 -> 0 ?
+    }
+
     nextTurn() {
-        this.turnsLeft = this.turnsLeft - 1
-        console.log(this.turnsLeft)
-    },
-    winConditions: [
-        [0,1,2],
-        [3,4,5],
-        [6,7,8],
-        [0,3,6],
-        [1,4,7],
-        [2,5,8],
-        [0,4,8],
-        [2,4,6],
-    ],
-    isGameFinished() {
+        this.checkForWinner()
+        if (this._gameContinues) {
+            this._isXsTurn = !this._isXsTurn
+            this._turn = this._turn + 1
+            console.log(this._turn)
+        }
+    }
+
+    checkForWinner() {
+        // add game finished/live boolean?
+
         // calculate if there is win or draw
+        if (this._gameContinues && this._turn > 4) { // check
+            // alert('checking!')
+            let win = this.calculateWinner()
+
+            document.querySelector('h2').innerText = `Checking! ${win}`
+        }
+
 
         // search win arrays for index of space just placed
-
+        //
         // output message
+
+    }
+
+    calculateWinner() {
+        return this._winConditions.some(winArr => {
+            const one = this[`space${winArr[0]}`].innerText
+            const two = this[`space${winArr[1]}`].innerText
+            const three = this[`space${winArr[2]}`].innerText
+            return one !== '' && one === two && two === three
+        })
+    }
+
+    endOfGame(){
+
+        this._gameContinues = false
     }
 }
 
-// start function
 function initPage() {
-    board.initBoard()
+    let board = new Board()
     console.log(board)
-
 }
-// separate init game function? (eventually...)
 
 initPage()
-
