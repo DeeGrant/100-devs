e84001ed2b6336279989c836e2fd77f3097a4543
Walter Grant <WalterGrant@gmail.com>
2021-03-11 17:33:40 -0600

    async-await practice

--- class27.5-materials/async-await/js/main.js
+++
@@ -1,6 +0,0 @@
-// Synchronous tasks execute on the first loop
-
-console.log('🌓 Synchronous 1')
-setTimeout(_ => console.log('🍅 Timeout 2'), 0)
-Promise.resolve().then(_ => console.log('🍍 Promise 3'))
-console.log('🌗 Synchronous 4')
