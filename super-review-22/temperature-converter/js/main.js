ee63e42ec4bab080ab0c5fbcb607887ce795e0e6
Dee Grant <WalterGrant@gmail.com>
2022-03-13 20:50:15 -0500

    celcius to fahrenheit

--- super-review-22/temperature-converter/js/main.js
+++ super-review-22/temperature-converter/js/main.js
@@ -1,1 +1,13 @@
 //Write your pseduo code first! 
+document.querySelector('button').addEventListener('click', CtoF)
+const span = document.querySelector('span')
+const input = document.querySelector('#celcius')
+
+function CtoF() {
+    const celcius = Number(input.value)
+    span.innerText = (celcius * 9 / 5 + 32).toString()
+}
+
+// 0 // 32
+// 100 // 212
+// -20 // -4
