0864cf00cdee4ea0370c0d2f6b6c3cdd0197f6f4
Dee Grant <WalterGrant@gmail.com>
2022-03-13 21:19:59 -0500

    loop 21 times

--- super-review-22/sir-savage-the-21st/js/main.js
+++ super-review-22/sir-savage-the-21st/js/main.js
@@ -1,2 +1,11 @@
 //Create a function that has a loop that prints '21' 21 times to the console and then call that function
 //Bonus can you make it print '21' 21 times to the dom?
+const says = document.querySelector('#savageSays')
+
+function sirSavage() {
+    for (let i = 0; i < 21; i++) {
+        says.innerText += '21-'
+    }
+}
+
+document.querySelector('img').addEventListener('click', sirSavage)
